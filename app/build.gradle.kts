e6af5b351b34127f9151e4936f342746fbf74fdc
Jasmine Allen <jaguilar@stu.jsu.edu>
2024-01-24 20:39:35 -0600

    Project setup. Enabled view binding and application features.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,10 @@ android {
     namespace = "com.example.lab01b"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.lab01b"
         minSdk = 22
--- app/src/main/java/com/example/lab01b/MainActivity.java
+++ app/src/main/java/com/example/lab01b/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.lab01b;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.lab01b.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.rockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
     }
 }
