34fa92e0e76663892b0bad53eed501cd30a7e058
Jasmine Allen <jaguilar@stu.jsu.edu>
2024-01-27 21:13:16 -0600

    added updatesScores and playRound functions.

--- app/src/main/java/com/example/lab01b/MainActivity.java
+++ app/src/main/java/com/example/lab01b/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 import com.example.lab01b.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    int playerScore = 0;
+    int computerScore = 0;
 
 
     @Override
@@ -19,68 +21,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         binding.rockButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                int playerScore = 0;
-                int computerScore = 0;
-                Weapon playerGuess = Weapon.ROCK;
-                TextView t = binding.guessOutput;
-                String text = "Player's Guess: " + playerGuess;
-                t.setText(text);
-                int randomNumber = (int)(Math.random() * 3);
-                Enum[] weaponArray = Weapon.values();
-                Enum computerGuess = weaponArray[randomNumber];
-                TextView t2 = binding.computerOutput;
-                String computer = "Computer's Guess: " + computerGuess;
-                t2.setText(computer);
-
-                TextView t3 = binding.scoreOutput;
-                TextView t4 = binding.reasonOutput;
-
-                if(computerGuess == Weapon.PAPER){
-                    computerScore++;
-                    String scoreText = "Player: " + playerScore + ", Computer: " + computerScore;
-                    String reasonText = "Computer win: Paper covers rock!";
-                    t3.setText(scoreText);
-                    t4.setText(reasonText);
-                }
-                else if(computerGuess == Weapon.SCISSORS){
-                    playerScore++;
-                    String scoreText = "Player: " + playerScore + ", Computer: " + computerScore;
-                    String reasonText = "Player win: Rock smashes Scissors!";
-                    t3.setText(scoreText);
-                    t4.setText(reasonText);
-                }
-                else{
-                    String scoreText = "Player: " + playerScore + ", Computer: " + computerScore;
-                    String reasonText = "Draw! Rock cannot beat Rock!";
-                    t3.setText(scoreText);
-                    t4.setText(reasonText);
-                }
+                playRound(Weapon.ROCK);
             }
         });
 
         binding.paperButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Weapon paper = Weapon.PAPER;
-                TextView t = binding.guessOutput;
-                String text = "Player's Guess: " + paper;
-                t.setText(text);
-                Log.d("WEAPON", "This is the weapon:" + paper);
+                playRound(Weapon.PAPER);
+
             }
         });
 
         binding.scissorsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Weapon scissors = Weapon.SCISSORS;
-                TextView t = binding.guessOutput;
-                String text = "Player's Guess: " + scissors;
-                t.setText(text);
-                Log.d("WEAPON", "This is the weapon:" + scissors);
+                playRound(Weapon.SCISSORS);
             }
         });
-
     }
+
+    private void updateScores(int playerScore, int computerScore) {
+        TextView t3 = binding.scoreOutput;
+        String scoreText = "Player: " + playerScore + ", Computer: " + computerScore;
+        t3.setText(scoreText);
+    }
+    private void playRound(Weapon playerGuess) {
+
+        TextView t = binding.guessOutput;
+        String text = "Player's Guess: " + playerGuess;
+        t.setText(text);
+
+        int randomNumber = (int) (Math.random() * 3);
+        Enum[] weaponArray = Weapon.values();
+        Enum computerGuess = weaponArray[randomNumber];
+        TextView t2 = binding.computerOutput;
+        String computer = "Computer's Guess: " + computerGuess;
+        t2.setText(computer);
+
+        TextView t3 = binding.scoreOutput;
+        TextView t4 = binding.reasonOutput;
+
+        if (playerGuess == computerGuess) {
+            String scoreText = "Player: " + playerScore + ", Computer: " + computerScore;
+            String reasonText = "Draw! " + playerGuess + " cannot beat " + playerGuess + "!";
+            t3.setText(scoreText);
+            t4.setText(reasonText);
+        }
+        else if ((playerGuess == Weapon.ROCK && computerGuess == Weapon.SCISSORS) ||
+                (playerGuess == Weapon.PAPER && computerGuess == Weapon.ROCK) ||
+                (playerGuess == Weapon.SCISSORS && computerGuess == Weapon.PAPER)) {
+            playerScore++;
+            updateScores(playerScore, computerScore);
+            String reasonText = "Player wins: " + playerGuess + " beats " + computerGuess + "!";
+            t4.setText(reasonText);
+        }
+        else {
+            computerScore++;
+            updateScores(playerScore, computerScore);
+            String reasonText = "Computer wins: " + computerGuess + " beats " + playerGuess + "!";
+            t4.setText(reasonText);
+        }
+    }
+
+
 }
